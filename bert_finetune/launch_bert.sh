9774cdfe82b331fa7920c446c61b0dd6ff9c8332
Chameleon Cloud User <cc@jie-haswell2-2.novalocal>
2019-11-13 03:33:01 +0000

    new file:   launch_bert.sh

---
+++ bert_finetune/launch_bert.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#Learning_Rate="0.00002 0.00003 0.00005"
+#Batch_Size="32 64"
+Learning_Rate="0.00005"
+Batch_Size="64"
+Task_Name="MRPC"
+Data_Dir_Name="MRPC"
+
+RESULT_DIR="result_dir-$Task_Name"
+mkdir $RESULT_DIR 2>/dev/zero
+program_home="`echo ~`"
+program="$program_home/bert/run_classifier.py \
+  --task_name=$Task_Name \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$GLUE_DIR/$Data_Dir_Name \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --num_train_epochs=1.0 \ "
+
+for lr in $Learning_Rate; do
+    echo "learning_rate=$lr"
+    for bs in $Batch_Size; do
+        echo "batch_size=$bs"
+        res_dir="$RESULT_DIR""/lr_$lr""_bs_$bs"
+        result="$res_dir""/log.txt"
+        FINISH_FILE="$res_dir""/__finished__"
+
+        if [ ! -d "$res_dir" ];then
+        	mkdir $res_dir 2>/dev/zero
+        	echo "" > $result
+        fi
+
+        APP_CMD="python $program --train_batch_size=$bs --learning_rate=$lr --output_dir=$res_dir"
+        echo "$APP_CMD" | tee -a $result
+        stdbuf -oL $APP_CMD 2>&1 | tee -a $result &
+        #stdbuf -oL $APP_CMD | tee $result &
+    done
+done
